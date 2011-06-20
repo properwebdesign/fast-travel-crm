ace5efdb2014f12015066481bc8041d6fd9e376e
5726612 <5726612@gmail.com>
2011-06-20 11:25:11 +0000

    Попытка создать web службу со своим собстенным механизмом доступа к данным

---
+++ FastTravel/WebSer/Hotel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSer
+{
+    public class HotelRepository
+    {
+        private FastTravelEntities context;
+
+        public HotelRepository()
+        {
+            context= new FastTravelEntities();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public List<Hotel> GetHotels()
+        {
+            return context.Hotels.OrderBy(h => h.Name).ToList();
+            //return this.context.Hotels.OrderBy(h => h.Name).ToList();
+        }
+
+    }
+}
